076107ca9b26d5ad0e0e10cd71bcfc7e1c4f6b0c
kennedybabu <kennedynjogu8@gmail.com>
2026-08-05 22:48:13 +0300

    ft: create customer model,basic folder structure

---
+++ src/main/java/api/model/Customer.java
@@ -0,0 +1,16 @@
+package api.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+}
